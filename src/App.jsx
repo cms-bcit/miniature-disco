1f1ac9ee908d0d9bea30d717cebba690997ae9f6
cms-bcit <christine_shiels@bcit.ca>
2025-08-06 17:09:47 -0700

    changed actions settings on gh

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
 					<Route index element={<Home />} />
 					<Route path='/parks' element={<Parks />} />
 					<Route path='/about' element={<About />} />
+          {/* test */}
 				</Route>
 			</Routes>
 		</BrowserRouter>
